20b72eeb49e762e69b98fa9dd5b3abafaabbd192
Hajime Hoshi <hajimehoshi@gmail.com>
2024-01-29 01:04:51 +0900

    hideconsole: add doc.go for documentation

--- hideconsole_notwindows.go
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: 2024 The Ebitengine Authors
-
-//go:build !windows
-
-package hideconsole
--- hideconsole_windows.go
+++ hideconsole_windows.go
@@ -1,16 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2024 The Ebitengine Authors
 
-// Package hideconsole is a utility package to hide a console automatically
-// even without `-ldflags "-Hwindowsgui"` on Windows.
-//
-// # Usage
-//
-// Import this package with a blank identifier:
-//
-//	import (
-//		_ "github.com/ebitengine/ebitengine/internal/hideconsole"
-//	)
 package hideconsole
 
 import (
